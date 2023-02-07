df37c9c4eb4065fe041442b5b947228fa4b630a5
Fatih <fatihcihan666@gmail.com>
2023-02-07 14:59:53 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Naming convention -> Camel case.
+var salaryAli = 7000;
+var salaryCan = 6000;
+var raise = 0.3;
+
+console.log(salaryAli + (salaryAli * raise));
+console.log(salaryCan + (salaryCan * raise));
